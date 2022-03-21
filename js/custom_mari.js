46f34efe7c43729330063f2cbed7c735c8ec6b84
sunsatgeeg <sjssj7777@naver.com>
2022-03-21 17:07:11 +0900

    .

--- js/custom_mari.js
+++ js/custom_mari.js
@@ -15,7 +15,46 @@ $(function(){
         dataType: 'jsonp',
         url: url + 'mari',
         success: function(json) {
-            console.log(json);
+            var newtime = [];
+            var prevtime = [];
+            var lasttime = [];
+
+            var temparr = [];
+            for (var i = 0; i < json.length; i++) {
+                temparr.push(json[i]);
+                if(temparr.length == 6){
+                    if(newtime.length == 0){
+                        newtime = temparr;
+                    }else if(prevtime.length == 0){
+                        prevtime = temparr;
+                    }else{
+                        lasttime = temparr;
+                    }
+                    temparr = [];
+                }
+            }
+            console.log(newtime);
+            console.log(prevtime);
+            console.log(lasttime);
+
+            var th = "";
+            var untilwhen = "";
+            var untilwhenday = "";
+            var untilwhenhour = "";
+            untilwhen = String(newtime[0]['untilwhen']);
+            untilwhenday = untilwhen.slice(0,2);
+            untilwhenhour = untilwhen.slice(-2);
+            $("#newtime").text(untilwhenday + "일 " + untilwhenhour + "시까지 판매");
+            untilwhen = String(prevtime[0]['untilwhen']);
+            untilwhenday = untilwhen.slice(0,2);
+            untilwhenhour = untilwhen.slice(-2);
+            $("#prevtime").text(untilwhenday + "일 " + untilwhenhour + "시까지 판매");
+            untilwhen = String(lasttime[0]['untilwhen']);
+            untilwhenday = untilwhen.slice(0,2);
+            untilwhenhour = untilwhen.slice(-2);
+            $("#lasttime").text(untilwhenday + "일 " + untilwhenhour + "시까지 판매");
+
+
         }
     });
 });
