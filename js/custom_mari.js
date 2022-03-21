fbe9f752e3c1f278de757cc4497ad229566fb298
sunsatgeeg <sjssj7777@naver.com>
2022-03-21 16:11:45 +0900

    .

---
+++ js/custom_mari.js
@@ -0,0 +1,21 @@
+local = false
+var url;
+var cookie;
+if(local){
+    cookie = '들꽃';
+    url = "http://127.0.0.1:5000/";
+}else{
+    cookie = $.cookie('indata');
+    url = "https://loamarketjson.herokuapp.com/";
+}
+
+$(function(){
+    $.ajax({
+        type: 'GET',
+        dataType: 'jsonp',
+        url: url + 'mari',
+        success: function(json) {
+            console.log(json);
+        }
+    });
+});
