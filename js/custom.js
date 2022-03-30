1743ec612baf4ce7231937e5e7875fd270bbc49b
sunsatgeeg <sjssj7777@naver.com>
2022-03-30 09:26:23 +0900

    .

--- js/custom.js
+++ js/custom.js
@@ -10,6 +10,14 @@ if(local){
     url = "https://lochart.ga/";
 }
 
+//ad
+if(false){
+    $('head').append(`
+    <!-- temp ADfit -->
+    <script src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
+    `);
+}
+
 function clickToCopy(element){
     try{value = $(this).val();}catch{};
 
@@ -35,7 +43,8 @@ $(function(){
     <a class="nav-link" aria-current="page" href="secretmap">비밀지도</a>
     <a class="nav-link" aria-current="page" href="bidcalc">경매 계산기</a>
     <a class="nav-link" aria-current="page" href="craftcalc">제작 계산기(미완)</a>
-    <a class="nav-link" aria-current="page" href="rewardcalc">더보기 계산기</a>`
+    <a class="nav-link" aria-current="page" href="rewardcalc">더보기 계산기</a>
+    <h1>현재 집에서 돌리고 있는 db 서버가 연결되지 않네요;;<br> 원인이 인터넷 연결이 끊어져있는데 갑자기?;;<br>출근길에 돌아가고 있는 것도 확인됐었는데<br> 19시 즈음에 퇴근하고 집에 도착해서<br> 인터넷 끊긴 원인 찾고 복구하겠습니다...</h1>`
     );
     
     $('header').find('.nav-link').filter(function(){
