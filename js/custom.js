49d3e7d360bd4d472804e0a34448fab320a4c8c8
한상길 <sjssj7777@naver.com>
2022-04-04 21:05:07 +0900

    .

--- js/custom.js
+++ js/custom.js
@@ -51,7 +51,7 @@ $(function(){
     <a class="nav-link" aria-current="page" href="mari">마리샵</a>
     <a class="nav-link" aria-current="page" href="secretmap">비밀지도</a>
     <a class="nav-link" aria-current="page" href="bidcalc">경매 계산기</a>
-    <a class="nav-link" aria-current="page" href="craftcalc">제작 계산기(미완)</a>
+    <a class="nav-link" aria-current="page" href="craftcalc">제작 계산기</a>
     <a class="nav-link" aria-current="page" href="rewardcalc">더보기 계산기</a>
     `);
     
