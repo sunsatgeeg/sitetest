31c712e1fc9dd5842a911169ac0fd93c871b1b69
sunsatgeeg <sjssj7777@naver.com>
2022-05-03 17:23:32 +0900

    .

--- test/js/cardcalc.js
+++ test/js/cardcalc.js
@@ -4,7 +4,7 @@ var carddeck = {};
 var cardqty = {};
 
 document.querySelector('#reconBtn').addEventListener('click', function(){
-    document.cookie = 'savecarddeck=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    delCookie('savecarddeck');
     location.reload();
 });
 
