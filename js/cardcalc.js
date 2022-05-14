442eee5c877263a2356ef489be2047a7b79703af
한상길 <sjssj7777@naver.com>
2022-05-14 20:40:43 +0900

    .

--- js/cardcalc.js
+++ js/cardcalc.js
@@ -482,14 +482,14 @@ var Module = {
                         
                         findx = 23
                         findy = 23
-                        console.log(cardlist[Object.keys(cardlist)[i]]);console.log(minMaxLoc)
+                        //console.log(cardlist[Object.keys(cardlist)[i]]);console.log(minMaxLoc)
                         if(cardlist[Object.keys(cardlist)[i]] == "코니"){
                             findx = 14;
                             findy = 8;
-                        }else if(cardlist[Object.keys(cardlist)[i]] == "벨루마테"){
-                            matchLoc = minMaxLoc.minLoc;
-                            findx = 20;
-                            findy = 78;
+                        }else if(cardlist[Object.keys(cardlist)[i]] == "샐리"){
+                            matchLoc  = minMaxLoc.minLoc;
+                            findx = 37;
+                            findy = 58;
                         }
                         if(matchLoc.x == findx && matchLoc.y == findy){
                             for (var l = 5; l > 0; l--) {
