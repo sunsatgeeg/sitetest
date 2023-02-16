80a490c2d1f7fa6a052f04436f86e1d9a720afe2
한상길 <sjssj7777@naver.com>
2023-02-16 00:52:28 +0000

    [EDIT] 도감작 계산기 > 카드 순서 변경

--- js/cardcalc/cardocr.js
+++ js/cardcalc/cardocr.js
@@ -80,7 +80,7 @@ let Module = {
             return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
         });
         
-        await loadJavascript('js/cardcalc/allCardImages.js?v=02152003');
+        await loadJavascript('js/cardcalc/allCardImages.js?v=02160950');
 
         copycardlist = Object.keys(cardlist);
         let cardAllQty = Object.keys(cardlist).length;
