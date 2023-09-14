ad388bc038de5cbfd3b6a69c7c2b3962b784d508
한상길 <sjssj7777@naver.com>
2023-09-14 12:01:19 +0900

    [ADD] 도감작 계산기 > 베스칼 추가 대응

--- js/cardcalc/cardocr.js
+++ js/cardcalc/cardocr.js
@@ -80,7 +80,7 @@ let Module = {
             return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
         });
         
-        await loadJavascript('js/cardcalc/allCardImages.js?v=08262013');
+        await loadJavascript('js/cardcalc/allCardImages.js?v=09141158');
 
         copycardlist = Object.keys(cardlist);
         let cardAllQty = Object.keys(cardlist).length;
